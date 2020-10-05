72bdbc9af846c21affb81c67cf171c3cb3cad6e0
GagaMen <12294922+GagaMen@users.noreply.github.com>
2020-10-05 09:42:45 +0200

    Optimize eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,25 @@
 module.exports = {
     root: true,
-    parser: '@typescript-eslint/parser',
-    plugins: [
-      '@typescript-eslint',
+    override: [
+        {
+            files: ['*.ts'],
+            excludeFiles: "*.d.ts",
+            parser: '@typescript-eslint/parser',
+            plugins: [
+              '@typescript-eslint',
+            ],
+            extends: [
+              'eslint:recommended',
+              'plugin:@typescript-eslint/recommended',
+              'plugin:import/recommended',
+              'plugin:import/stage-0',
+              'plugin:import/errors',
+              'plugin:import/warnings',
+              'plugin:import/typescript'
+            ],
+            rules: {
+                'import/no-cycle': 'error',
+            },
+        }
     ],
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:import/recommended',
-      'plugin:import/stage-0',
-      'plugin:import/errors',
-      'plugin:import/warnings',
-      'plugin:import/typescript'
-    ],
-    rules: {
-        'import/no-cycle': 'error',
-    },
   };
