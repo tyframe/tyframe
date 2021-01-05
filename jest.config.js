898daa4531a65859228b23ef76ef34991f728f2b
GagaMen <12294922+GagaMen@users.noreply.github.com>
2021-01-05 21:32:14 +0100

    Uncomment unit test coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -8,13 +8,13 @@ module.exports = {
     collectCoverage: true,
     coverageReporters: ['lcov', 'json'],
     collectCoverageFrom: ['packages/**/src/**', '!packages/example/src/**', '!packages/**/*.d.ts', '!packages/**/src/index.ts'],
-    // coverageThreshold: {
-    //     global: {
-    //         branches: 90,
-    //         functions: 90,
-    //         lines: 90,
-    //         statements: 90,
-    //     },
-    // },
+    coverageThreshold: {
+        global: {
+            branches: 90,
+            functions: 90,
+            lines: 90,
+            statements: 90,
+        },
+    },
     testRegex: '(/test/.*|(\\.|/)spec)\\.ts$',
 };
